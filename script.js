68b7a55f1873cb496f869d738b1ace9cfd405f16
Enofua Divine <divineenofua@gmail.com>
2022-07-23 01:22:39 -1200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const showNav = document.querySelector('#open-menu-btn');
+const closeNav = document.querySelector('#close-menu-btn');
+const menu = document.querySelector('.nav_menu');
+showNav.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeNav.style.display = 'inline-block';
+    showNav.style.display = 'none';
+})
+closeNav.addEventListener('click', () => {
+    menu.style.display = 'none';
+    showNav.style.display = 'inline-block';
+    closeNav.style.display = 'none'
+
+})
